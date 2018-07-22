1e67f36f6fb9296395cf358d493df0b3fe95867e
wuzeng <jiushiqiangone@sina.com>
2018-07-22 23:22:28 +0800

    7.22 home

---
+++ app/src/main/java/com/system/bhouse/Custom/View/SceneSurface/SceneSurfaceView.java
@@ -0,0 +1,82 @@
+package com.system.bhouse.Custom.View.SceneSurface;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+
+/**
+ * Created by wz on 2018/7/22.
+ */
+
+public class SceneSurfaceView extends SurfaceView implements SurfaceHolder.Callback {
+
+    private RenderThread renderThread;
+    private SurfaceHolder surfaceHolder;
+
+    int width;
+    int height;
+
+    public SceneSurfaceView(Context context) {
+        super(context);
+        init();
+    }
+
+    public SceneSurfaceView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init();
+    }
+
+    public SceneSurfaceView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init();
+    }
+
+    private void init() {
+        surfaceHolder = getHolder();
+        surfaceHolder.addCallback(this);
+
+        setFocusable(true);
+        setFocusableInTouchMode(true);
+        this.setKeepScreenOn(true);
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        width = getMeasuredWidth();
+        height = getMeasuredHeight();
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+        Log.d("weather", "onMeasure width=" + width + ",height=" + height);
+        if (renderThread != null) {
+            renderThread.setWidth(width);
+            renderThread.setHeight(height);
+        }
+    }
+
+
+    @Override
+    public void surfaceCreated(SurfaceHolder holder) {
+        Log.e("weather", "surfaceCreated");
+        if (renderThread == null) {
+            renderThread = new RenderThread(surfaceHolder, getContext());
+            renderThread.start();
+        }
+    }
+
+    @Override
+    public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+
+    }
+
+    @Override
+    public void surfaceDestroyed(SurfaceHolder holder) {
+        renderThread.getRenderHandler().sendEmptyMessage(1);
+    }
+
+    @Override
+    protected void onFinishInflate() {
+        super.onFinishInflate();
+        Log.d("weather", "onFinishInflate");
+    }
+}
--- app/src/main/java/com/system/bhouse/utils/MeasureUtil.java
+++ app/src/main/java/com/system/bhouse/utils/MeasureUtil.java
@@ -18,6 +18,7 @@ import android.view.WindowManager;
 import android.widget.ListAdapter;
 import android.widget.ListView;
 
+import com.system.bhouse.base.App;
 import com.system.bhouse.bhouse.R;
 
 import java.text.ParseException;
@@ -53,7 +54,6 @@ public class MeasureUtil {
     /**
      * 获取ToolBar的高度
      *
-     * @param context
      * @return
      */
 //    public static int getToolbarHeight(Context context) {
@@ -302,5 +302,47 @@ public class MeasureUtil {
         return output;
     }
 
+    /**
+     * 设置某个View的margin
+     *
+     * @param view   需要设置的view
+     * @param isDp   需要设置的数值是否为DP
+     * @param left   左边距
+     * @param right  右边距
+     * @param top    上边距
+     * @param bottom 下边距
+     * @return
+     */
+    public static ViewGroup.LayoutParams setViewMargin(View view, boolean isDp, int left, int right, int top, int bottom) {
+        if (view == null) {
+            return null;
+        }
+
+        int leftPx = left;
+        int rightPx = right;
+        int topPx = top;
+        int bottomPx = bottom;
+        ViewGroup.LayoutParams params = view.getLayoutParams();
+        ViewGroup.MarginLayoutParams marginParams = null;
+        //获取view的margin设置参数
+        if (params instanceof ViewGroup.MarginLayoutParams) {
+            marginParams = (ViewGroup.MarginLayoutParams) params;
+        } else {
+            //不存在时创建一个新的参数
+            marginParams = new ViewGroup.MarginLayoutParams(params);
+        }
+
+        //根据DP与PX转换计算值
+        if (isDp) {
+            leftPx = dip2px(App.getContextApp(),left);
+            rightPx = dip2px(App.getContextApp(),right);
+            topPx = dip2px(App.getContextApp(),top);
+            bottomPx = dip2px(App.getContextApp(),bottom);
+        }
+        //设置margin
+        marginParams.setMargins(leftPx, topPx, rightPx, bottomPx);
+        view.setLayoutParams(marginParams);
+        return marginParams;
+    }
 
 }
